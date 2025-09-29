17ea3b5ee746f62d333fd3396bdacf67aaf2b19d
Charmaine Coloma <charmainejoycecoloma02@gmail.com>
2025-09-29 16:58:06 +0800

    feat(home): add About section and Footer in Home.tsx

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Hero from "./Hero/Hero";
-import Destination from "./Destination/Destination";
 import Hotel from "@/components/Hotel/Hotel";
+import Footer from "@/components/Footer/Footer";
+import About from "../About/About";
 
 const Home = () => {
   return (
     <div className="overflow-hidden">
       <Hero />
-      <Destination />
+      <About />
       <Hotel />
+      <Footer />
     </div>
   );
 };
