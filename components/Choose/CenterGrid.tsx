7c8bc34eacfcf227fa0da002607d05f14c4c70bd
Charmaine Coloma <charmainejoycecoloma02@gmail.com>
2025-09-30 14:01:33 +0800

    refactor(home): modify Choose section and remove unused code

--- components/Choose/CenterGrid.tsx
+++ components/Choose/CenterGrid.tsx
@@ -3,13 +3,14 @@ import ExperienceCard from "./ExperienceCard";
 
 const CenterGrid = () => {
   return (
-    <div className="lg:col-span-4 grid grid-cols-2 grid-rows-2 gap-6">
-      <div className="bg-gray-300 rounded-2xl h-53"></div>
-      <div className="bg-gray-300 rounded-2xl h-53"></div>
+    <div className="lg:col-span-4 grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 
+                    max-w-sm sm:max-w-md mx-auto lg:max-w-none lg:mx-0">
+      <div className="bg-gray-300 rounded-2xl aspect-square sm:aspect-auto sm:h-[180px] md:h-[200px] lg:h-[220px]"></div>
+      <div className="bg-gray-300 rounded-2xl aspect-square sm:aspect-auto sm:h-[180px] md:h-[200px] lg:h-[220px]"></div>
       <div>
         <ExperienceCard />
       </div>
-      <div className="bg-gray-300 rounded-2xl h-53"></div>
+      <div className="bg-gray-300 rounded-2xl aspect-square sm:aspect-auto sm:h-[180px] md:h-[200px] lg:h-[220px]"></div>
     </div>
   );
 };
--- components/Choose/Choose.tsx
+++ components/Choose/Choose.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import ChooseHeader from './ChooseHeader';
-import ChooseGrid from './ChooseGrid';
+import ChooseContent from './ChooseContent';
 
 const Choose = () => {
   return (
     <div className="pt-20 pb-20 bg-[#f8f8f8] relative overflow-hidden">
-      {/* Background Pattern */}
+      {/* Background Pattern - Responsive */}
       <div className="absolute top-0 right-0 w-1/2 h-full opacity-[0.03]">
-        <div className="absolute top-20 right-0 w-[500px] h-[500px] rounded-full border-[80px] border-gray-300"></div>
+        <div className="absolute top-10 right-0 w-[300px] h-[300px] sm:w-[500px] sm:h-[500px] rounded-full border-[60px] sm:border-[80px] border-gray-300"></div>
       </div>
 
       <div className="w-[90%] lg:w-[85%] xl:w-[80%] mx-auto relative z-10">
-        <ChooseHeader />
-        <ChooseGrid />
+        <ChooseContent />
       </div>
     </div>
   );
