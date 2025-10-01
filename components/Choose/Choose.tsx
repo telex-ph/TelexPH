91b8103ba78d885633471acfcf99b39a88ba7f3a
Charmaine Coloma <charmainejoycecoloma02@gmail.com>
2025-10-01 10:48:13 +0800

    fix(ui): adjust scroll padding top for proper page navigation

--- components/Choose/Choose.tsx
+++ components/Choose/Choose.tsx
@@ -3,7 +3,10 @@ import ChooseContent from './ChooseContent';
 
 const Choose = () => {
   return (
-    <div id="choose" className="pt-20 pb-20 bg-[#f8f8f8] relative overflow-hidden">
+    <div
+      id="choose"
+      className="pt-20 pb-20 bg-[#f8f8f8] relative overflow-hidden scroll-mt-[120px]"
+    >
       {/* Background Pattern - Responsive */}
       <div className="absolute top-0 right-0 w-1/2 h-full opacity-[0.03]">
         <div className="absolute top-10 right-0 w-[300px] h-[300px] sm:w-[500px] sm:h-[500px] rounded-full border-[60px] sm:border-[80px] border-gray-300"></div>
