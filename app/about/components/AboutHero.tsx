99933e87a136cb75376345c64c6d4fdca21e71fc
Charmaine Coloma <charmainejoycecoloma02@gmail.com>
2025-10-01 10:26:29 +0800

    chore: remove unnecessary comment

--- app/about/components/AboutHero.tsx
+++ app/about/components/AboutHero.tsx
@@ -2,28 +2,19 @@ import React from "react";
 import Link from "next/link";
 
 export default function AboutHero() {
-  // Ang color palette base sa bagong design ay puting background, 
-  // dark gray/black text, at red accent.
 
   return (
-    // Binawasan ang padding sa ibaba mula pb-20 papuntang pb-16.
     <section className="relative bg-white text-gray-900 pt-40 pb-16 overflow-hidden">
       
-      {/* Background Outline Text - "Our About" (Black Outline) */}
       <div 
-          // Binawasan pa ang font size (mula 10rem ginawang 8rem sa large screen)
-          // at inangat para mas mataas ang kalahati ng text.
           className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[85%] 
                      text-[5rem] md:text-[7rem] lg:text-[8rem] 
                      opacity-10 font-extrabold select-none pointer-events-none 
                      leading-none z-0 whitespace-nowrap" 
-                     // Ginamit ang -translate-y-[85%] para mas maangat at lumiit ang text.
                      
       >
           <span 
-            // Custom CSS for outline text with black stroke and transparent fill
             style={{
-              // Ginawang itim (black) ang stroke at ginawang mas makapal (2.5px)
               WebkitTextStroke: '2.5px #333333', 
               WebkitTextFillColor: 'transparent', 
               opacity: 0.9, 
