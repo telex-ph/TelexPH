9733263353032d352c385bbf5e8c65e6216769ac
Charmaine Coloma <charmainejoycecoloma02@gmail.com>
2025-10-01 08:44:48 +0800

    refactor(home): update home page and constants

--- constant/constant.ts
+++ constant/constant.ts
@@ -3,27 +3,40 @@ import { url } from "inspector";
 export const navLinks = [
     {
         id:1,
-        url:"#",
+        url:"#home", // Anchor Link: Home section (gagamitin natin ito sa mobile para mag-scroll sa taas)
         label:"Home"
     },
     {
         id:2,
-        url:"#",
-        label:"About"
+        url:"#about", // Anchor Link: About section sa homepage
+        label:"About",
+        // Idinagdag ang dropdown
+        dropdown: [
+            {
+                id: 201,
+                url: "/about", // Ito ang magli-link sa app/about/page.tsx (About Overview)
+                label: "About Overview"
+            }
+        ]
     },
     {
         id:3,
-        url:"#",
+        url:"#services",
         label:"Services"
     },
     {
         id:4,
-        url:"#",
+        url:"#industry",
         label:"Industry"
     },
     {
         id:5,
-        url:"#",
+        url:"#careers",
         label:"Careers"
+    },
+    {
+        id:6,
+        url:"#choose", // Anchor Link: Why Choose Us section
+        label:"Why Choose Us" 
     }
 ]
