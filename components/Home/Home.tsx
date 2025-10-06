81c7a2e2d23a904ae7a4f607b635d4b04ebfa5c4
Charmaine Coloma <charmainejoycecoloma02@gmail.com>
2025-10-06 09:51:10 +0800

    refactor(home): restructure sections order

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,26 +1,37 @@
+// Home.tsx (assuming this is app/page.tsx or pages/index.tsx)
+
 import React from "react";
-import Hero from "./Hero/Hero";
+// Assuming these are the paths to your components
+import Hero from "./Hero/Hero"; 
 import Choose from "@/components/Choose/Choose";
 import Footer from "@/components/Footer/Footer";
-import About from "../About/About";
+// Make sure the path to your About component is correct. 
+import Partners from "../Partners/Partners";
+import AboutUs from "../AboutUs/AboutUs";
 
 const Home = () => {
   return (
     <div className="overflow-hidden">
+      
       {/* 1. Hero Section - ID: home */}
       <div id="home">
         <Hero />
       </div>
       
-      {/* 2. About Section - ID: about */}
-      {/* <div id="about">
-        <About />
-      </div> */}
+      {/* 2. Partners Section - ID: partners (Moved right after Hero) */}
+      <div id="partners">
+        <Partners />
+      </div>
       
-      {/* 3. Choose Section - ID: choose */}
+      {/* 3. Choose Section - ID: choose (Commented out as requested) */}
       {/* <div id="choose">
         <Choose />
       </div> */}
+      
+      {/* 4. About Section - ID: about (Moved before Footer) */}
+      <div id="about">
+        <AboutUs />
+      </div>
       
       <Footer />
     </div>
