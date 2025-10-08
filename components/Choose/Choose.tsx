f47c8ad6e7608e0cae69beaaf5769c36b3d4fa53
Charmaine Coloma <charmainejoycecoloma02@gmail.com>
2025-10-09 06:22:27 +0800

    refactor(choose): define DEFAULT_MAX_WIDTH_CLASS locally instead of importing from constants

--- components/Choose/Choose.tsx
+++ components/Choose/Choose.tsx
@@ -1,20 +1,18 @@
 import React from "react";
 import ChooseContent from "./ChooseContent";
-import { DEFAULT_MAX_WIDTH_CLASS } from "@/constant/layout"; 
+const DEFAULT_MAX_WIDTH_CLASS = "w-full max-w-[1400px] mx-auto px-4 sm:px-6 lg:px-8";
 
 const Choose = () => {
-  const backgroundImageStyle = { 
-    backgroundImage: "url('/images/choosebg.jpg')" 
-  };
-  
   return (
     <div id="choose" className="pt-20 pb-20 relative overflow-hidden">
       <div
         className="absolute inset-0 w-full h-full bg-cover bg-center"
-        style={backgroundImageStyle}
+        style={{ backgroundImage: "url('/images/choosebg.jpg')" }}
       ></div>
       <div className="absolute inset-0 bg-black/20"></div>
+
       <div className="absolute inset-0 bg-white/90"></div>
+
       <div className={DEFAULT_MAX_WIDTH_CLASS + " relative z-10"}>
         <ChooseContent />
       </div>
