b73bd4653365cf0eccb095b9b6ad817108f0d9b6
Charmaine Coloma <charmainejoycecoloma02@gmail.com>
2025-10-10 14:28:11 +0800

    chore(navbar): temporarily comment out dropdown menu functionality

--- constant/constant.ts
+++ constant/constant.ts
@@ -1,31 +1,36 @@
 import { url } from "inspector";
 
 export const navLinks = [
-    {
-        id:1,
-        url:"/",
-        label:"Home"
-    },
-    {
-        id:2,
-        url:"#about",
-        label:"About",
-    },
-    {
-        id:3,
-        url:"#services", 
-        label:"Services",
-        dropdown: [
-            {
-                id: 4,
-                url: "#choose", 
-                label: "Industry"
-            }
-        ]
-    },
-    {
-        id:5,
-        url:"#process",
-        label:"Careers"
-    },
-]
+  {
+    id: 1,
+    url: "/",
+    label: "Home",
+  },
+  {
+    id: 2,
+    url: "#about",
+    label: "About",
+  },
+  {
+    id: 3,
+    url: "#services",
+    label: "Services",
+    // dropdown: [
+    //   {
+    //     id: 4,
+    //     url: "#choose",
+    //     label: "Industry",
+    //   },
+    // ],
+  },
+  {
+    id: 4,
+    url: "#choose",
+    label: "Industry",
+  },
+  {
+    id: 5,
+    url: "#process",
+    label: "Careers",
+  },
+];
