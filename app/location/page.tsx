07d4bdc1dfbe31c07046a46a6933b730009cd3ea
Charmaine Coloma <charmainejoycecoloma02@gmail.com>
2025-10-17 14:07:20 +0800

    refactor(LocationPage): remove unused contact section for cleaner codebase

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -2,8 +2,6 @@
 
 import React from 'react';
 import LocationHero from './components/LocationHero';
-import LocationContactInfo from './components/LocationContactInfo';
-import LocationContactForm from './components/LocationContactForm';
 import LocationMap from './components/LocationMap';
 import Footer from '@/components/Footer/Footer';
 import { COLORS, FONTS, TYPOGRAPHY } from '@/constant/styles';
@@ -19,19 +17,6 @@ const LocationPage = () => {
     >
       {/* Hero Section */}
       <LocationHero />
-
-      {/* Contact Section - Two Columns */}
-      <section className="py-16" style={{ backgroundColor: '#f9fafb' }}>
-        <div className="max-w-[1400px] mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
-            {/* Left: Contact Info */}
-            <LocationContactInfo />
-
-            {/* Right: Contact Form */}
-            <LocationContactForm />
-          </div>
-        </div>
-      </section>
 
       {/* Map Section - Full Width Bottom */}
       <section className="py-16">
